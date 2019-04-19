149d2cae5eed6ce1cfe9de4e8af8649a003d4a60
Eric Lugo <ericlugo@users.noreply.github.com>
2019-04-19 02:15:27 -0400

    Stretch 1 Complete

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -1,62 +1,98 @@
+/* <== STRETCH WORK ==> */
+class Tabs {
+  constructor(className) {
+    this.tabs = document.querySelectorAll(`${className}`);
+    this.tabList = [];
+    this.tabs.forEach((tab) => {
+      this.tabList.push(new TabLink(tab));
+      if (tab.classList.contains('tabs-link-selected'))
+        this.currentSelection = tab.dataset.tab;
+    });
+    document.body.addEventListener('click', (event) => this.select(event));
+  }
+
+  select(event) {
+    if (event.target.classList.contains('tabs-link')) {
+      if (event.target.dataset.tab !== this.currentSelection) {
+        this.tabList[this.currentSelection - 1].deselect();
+        this.currentSelection = event.target.dataset.tab;
+        this.tabList[this.currentSelection - 1].select();
+      }
+    }
+  }
+}
+
 class TabLink {
   constructor(element) {
-    // Assign this.element to the passed in DOM element
     this.element = element;
-    // Get the custom data attribute on the Link
     this.data = this.element.dataset.tab;
-    // Using the custom data attribute get the associated Item element
     this.itemElement = document.querySelector(
       `.tabs-item[data-tab = '${this.data}']`,
     );
-    // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
-    // console.log(this.tabItem);
-    // Add a click event listener on this instance, calling the select method on click
-    this.element.addEventListener('click', () => this.select());
   }
 
   select() {
-    // Get all of the elements with the tabs-link class
-    const links = document.querySelectorAll('.tabs-link');
-    // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    Array.from(links).forEach((link) =>
-      link.classList.remove('tabs-link-selected'),
-    );
-    // Add a class named "tabs-link-selected" to this link
     this.element.classList.add('tabs-link-selected');
-    // Call the select method on the item associated with this link
     this.tabItem.select();
+  }
+
+  deselect() {
+    this.element.classList.remove('tabs-link-selected');
+    this.tabItem.deselect();
   }
 }
 
 class TabItem {
   constructor(element) {
-    // Assign this.element to the passed in element
     this.element = element;
   }
 
   select() {
-    // Select all ".tabs-item" elements from the DOM
-    const items = document.querySelectorAll('.tabs-item');
-    // Remove the class "tabs-item-selected" from each element
-    Array.from(items).forEach((item) =>
-      item.classList.remove('tabs-item-selected'),
-    );
-    // Add a class named "tabs-item-selected" to this element
     this.element.classList.add('tabs-item-selected');
+  }
+
+  deselect() {
+    this.element.classList.remove('tabs-item-selected');
   }
 }
 
-/* START HERE: 
+tabs = new Tabs('.tabs-link');
 
-- Select all classes named ".tabs-link" and assign that value to the links variable
+/* MVP ITEMS BELOW */
+/* class TabLink {
+  constructor(element) {
+    this.element = element;
+    this.data = this.element.dataset.tab;
+    this.itemElement = document.querySelector(
+      `.tabs-item[data-tab = '${this.data}']`,
+    );
+    this.tabItem = new TabItem(this.itemElement);
+    this.element.addEventListener('click', () => this.select());
+  }
 
-- With your selection in place, now chain a .forEach() method onto the links variable to iterate over the DOM NodeList
+  select() {
+    const links = document.querySelectorAll('.tabs-link');
+    Array.from(links).forEach((link) =>
+      link.classList.remove('tabs-link-selected'),
+    );
+    this.element.classList.add('tabs-link-selected');
+    this.tabItem.select();
+  }
+} */
 
-- In your .forEach() method's callback function, return a new instance of TabLink and pass in each link as a parameter
+/* class TabItem {
+  constructor(element) {
+    this.element = element;
+  }
 
-*/
+  select() {
+    const items = document.querySelectorAll('.tabs-item');
+    Array.from(items).forEach((item) =>
+      item.classList.remove('tabs-item-selected'),
+    );
+    this.element.classList.add('tabs-item-selected');
+  }
+} */
 
-links = document
-  .querySelectorAll('.tabs-link')
-  .forEach((link) => new TabLink(link));
+// links = document.querySelectorAll('.tabs-link').forEach((link) => new TabLink(link));
